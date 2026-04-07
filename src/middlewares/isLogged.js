51d91d4714d4d14ff8824b253c9d2ce0ca6de2df
DicksonNdumia <ndumiadickson19@gmail.com>
2026-04-07 14:49:31 +0300

    f

--- src/middlewares/isLogged.js
+++ src/middlewares/isLogged.js
@@ -1,6 +1,39 @@
-//writing my own middleware to  show the endpoint hit
-export const myLogger = function (req, res, next) {
-  console.log(`http://localhost:3000${req.url} `);
+import chalk from "chalk";
+
+export const myLogger = (req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+
+    // Status color
+    const statusColor =
+      res.statusCode >= 500
+        ? chalk.red
+        : res.statusCode >= 400
+          ? chalk.yellow
+          : res.statusCode >= 300
+            ? chalk.cyan
+            : chalk.green;
+
+    // Method color
+    const methodColor =
+      req.method === "GET"
+        ? chalk.blue
+        : req.method === "POST"
+          ? chalk.magenta
+          : req.method === "PUT"
+            ? chalk.cyan
+            : req.method === "DELETE"
+              ? chalk.red
+              : chalk.white;
+
+    console.log(
+      `${methodColor(req.method)} ${chalk.white(`http://localhost:3000${req.url}`)} ` +
+        statusColor(res.statusCode) +
+        chalk.gray(` - ${duration}ms`),
+    );
+  });
 
   next();
 };
